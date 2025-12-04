48d5af326e6d1c2effc7604bb4e45e800a5d7e82
celveza <adrielip2007@gmail.com>
2025-12-04 01:19:10 +0100

    test sunshine configs

--- modules/sunshine.nix
+++ modules/sunshine.nix
@@ -1,8 +1,22 @@
+{ pkgs, nur, ... }:
 {
 	services.sunshine = {
 		enable = true;
 		autoStart = true;
 		capSysAdmin = true;
 		openFirewall = true;
+		settings = {
+			sunshine_name = "celveza";
+		};
+		applications = {
+			apps = [
+				{
+					name = "Jellyfin";
+					cmd = "jellyfinmediaplayer";
+					detached = true;
+					auto-detach = true;
+				}
+			];
+		};
 	};
 }
