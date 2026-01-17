6b716f1d2a64487122bbf20d215df8a5519bd711
celveza <adrielip2007@gmail.com>
2026-01-17 18:17:27 +0100

    Created a phpmyadmin server to learn mysql

--- modules/sql.nix
+++ modules/sql.nix
@@ -1,4 +1,4 @@
-{ pkgs, ...}:
+{ pkgs, config, ...}:
 {
 	services.mysql = {
 		enable = true;
@@ -14,11 +14,17 @@
 	virtualisation.oci-containers.containers = {
     	phpmyadmin = {
       		image = "phpmyadmin/phpmyadmin";
-      		ports = [ "8080:80" ]; # Accederás por el puerto 8080
 	      environment = {
 	        PMA_HOST = "host.containers.internal"; 
 	        PMA_PORT = "3306";
+			PMA_USER = "admin";
+			PMA_HIDE_DB = "^(information_schema|mysql|performance_schema|sys)$";
 	      };
+
+		  environmentFiles = [
+    		config.sops.secrets.pma_env.path
+  		  ];
+
 	      extraOptions = [ "--network=host" ]; # Usa la red del host para ver tu MariaDB local fácilmente
     	};
   	};
